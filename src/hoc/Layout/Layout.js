85337ff373f8a1e31c9fe2125179665e5d8b61c3
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-06-21 20:59:54 +0300

    initial setup

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Aux from '../Aux/Aux';
+const layout = (props) => {
+  return (
+    <Aux>
+      <div>Toolbar, Sidebar, Backdrop</div>
+      <main>{props.children}</main>
+    </Aux>
+  );
+};
+
+export default layout;
